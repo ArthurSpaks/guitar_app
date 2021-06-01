461330a7ded6e218497a3cd158319f33fa6ef218
ArthurShpak <a.spaks@newcastle,ac.uk>
2021-06-01 19:47:29 +0100

    1

--- app/src/main/java/com/example/guitarapplication/ChosenSongFragment.kt
+++ app/src/main/java/com/example/guitarapplication/ChosenSongFragment.kt
@@ -25,6 +25,7 @@ class ChosenSongTabsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_chosen_song, container, false)
